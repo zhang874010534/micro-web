43f1923dc8c2c3bd1ea3fbb34403e44526829461
zhangce <z874010534@vip.qq.com>
2022-03-15 20:57:19 +0800

    new

---
+++ build/run.js
@@ -0,0 +1,40 @@
+const childProcess = require('child_process');
+const path = require('path');
+const os = require('os');
+
+function joinPath(name) {
+	return path.join(__dirname, '../' + name);
+}
+const filePath = {
+	vue2: joinPath('vue2'),
+	vue3: joinPath('vue3'),
+	react17: joinPath('react17'),
+	// 'react15': joinPath('react15'),
+	service: joinPath('service'),
+};
+
+function runChild() {
+	let portShell = ''
+	if (os.type() == 'Windows_NT') {
+		// windows
+		portShell = `set`
+	} else if (os.type() == 'Darwin') {
+		//mac
+		portShell = ``
+	} else if (os.type() == 'Linux') {
+		//Linux
+	} else{
+		//不支持提示
+	}
+	Object.values(filePath).forEach((item, index) => {
+		// childProcess.spawn(`cd ${item} && ${portShell} PORT=${8080 + index}&& npm run start`, {
+		// 	stdio: 'inherit',
+		// 	shell: true
+		// });
+		childProcess.spawn(`cd ${item} && npm run start`, {
+			stdio: 'inherit',
+			shell: true
+		});
+	});
+}
+runChild();
--- service/routes/vue3.js
+++ service/routes/vue3.js
@@ -3,6 +3,7 @@ const router = require('koa-router')()
 router.prefix('/vue3')
 
 router.get('/car', async (ctx) => {
+    console.log(ctx.request.query)
     ctx.body = {
         title: 1234
     }
