3b331295f0775e11c78b8ee7a016dd2fe6c4a86d
zhangce <z874010534@vip.qq.com>
2022-03-16 23:32:30 +0800

    feat: vue2项目全局挂载 window.vue2 webpack5

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -4,9 +4,27 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+const render = () => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+}
+// render()
+if(!window.__MICRO_WEB__) {
+  render()
+}
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// 开始加载结构
+export const boot = () => {
+  console.log('开始加载')
+}
+
+export const mount = () => {
+  console.log('渲染成功')
+}
+
+export const unmount = () => {
+  console.log('卸载成功')
+}
--- vue3/src/views/HomeView.vue
+++ vue3/src/views/HomeView.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
