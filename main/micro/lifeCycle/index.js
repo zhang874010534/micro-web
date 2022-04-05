7eb3db53346f5c50c2e89a7e78e0b5705826b052
zhangce <z874010534@vip.qq.com>
2022-04-05 11:29:25 +0800

    feat: 微前端生命周期

--- main/micro/lifeCycle/index.js
+++ main/micro/lifeCycle/index.js
@@ -1,8 +1,44 @@
 import {findAppByRoute} from "../util";
-export const lifeCycle = () => {
+import {getMainLifeCycle} from "../const/mainLifeCycle";
+export const lifeCycle = async () => {
   // 获取到上一个子应用
   const prevApp = findAppByRoute(window.__ORIGIN_APP__)
   // 获取到要跳转到的子应用
   const nextApp = findAppByRoute(window.__CURRENT_SUB_APP__)
-  console.log(prevApp,nextApp,'appppp')
+  console.log(prevApp,nextApp,'app')
+  if(!nextApp) {
+    return
+  }
+  if(prevApp && prevApp.destroyed) {
+    await destroyed(prevApp)
+  }
+
+  const app = await beforeLoad(nextApp)
+
+  await mounted(app)
+}
+
+export const beforeLoad = async (app) => {
+  await runMainLiftCycle('beforeLoad')
+  app && app.beforeLoad && app.beforeLoad()
+
+  const appContext = null
+  return appContext
+}
+
+export const mounted = async (app) => {
+  app && app.mounted()
+
+  await runMainLiftCycle('mounted')
+}
+
+export const destroyed = async (app) => {
+  app && app.destroyed && app.destroyed()
+  // 对应的执行以下主应用的生命周期
+  await runMainLiftCycle('destroyed')
+}
+
+export const runMainLiftCycle = async (type) => {
+  const mainLife = getMainLifeCycle()
+  await Promise.all(mainLife[type].map(async item => await item()))
 }
